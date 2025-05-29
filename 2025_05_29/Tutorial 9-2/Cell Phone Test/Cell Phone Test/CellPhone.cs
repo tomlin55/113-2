6d73bd48bff170c30118de24bac9c864a3cc4840
林昀鋒 <A113221053@mail.shu.edu.tw>
2025-05-29 13:00:58 +0800

    2025_05_29

---
+++ 2025_05_29/Tutorial 9-2/Cell Phone Test/Cell Phone Test/CellPhone.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cell_Phone_Test
+{
+    class CellPhone
+    {
+        private string manufacturer;               
+    }
+}
